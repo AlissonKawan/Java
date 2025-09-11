b2588b333fb580de4b905dfc79a9afe5b7f8db68
AlissonKawan <rm567598@fiap.com.br>
2025-09-11 19:48:31 -0300

    commit

---
+++ Java_aula/src/Construtores/Aluno.java
@@ -0,0 +1,22 @@
+package Construtores;
+
+public class Aluno {
+    //atributos
+    private int matricula;
+    private String nome;
+    private String curso;
+    private boolean status;
+    private float mediaGeral;
+
+    //construtor
+
+
+    //getter and setters
+
+    public int getMatricula(){
+        return this.matricula;
+    }
+    public void setMatricula(int matricula){
+        this.matricula = matricula;
+    }
+}
