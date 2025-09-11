cbe696d13933ddaf2c7203289136b2fa6bba7c31
AlissonKawan <rm567598@fiap.com.br>
2025-09-11 19:52:24 -0300

    commit

--- Java_aula/src/Construtores/Aluno.java
+++ Java_aula/src/Construtores/Aluno.java
@@ -19,4 +19,10 @@ public class Aluno {
     public void setMatricula(int matricula){
         this.matricula = matricula;
     }
+    public String getNome(){
+        return  this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
 }
