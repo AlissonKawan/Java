2d7adb9c31c3056d56a3fc40988a03f78d32075a
AlissonKawan <rm567598@fiap.com.br>
2026-03-09 19:59:13 -0300

    Mudando Factory

--- ProjetoTDBResponde/src/connection/ConnectionFactory.java
+++ ProjetoTDBResponde/src/connection/ConnectionFactory.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class giConnectionFactory {
+public class ConnectionFactory {
 
     private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
     private static final String USUARIO = "rm567598";
