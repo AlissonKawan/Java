df9a2d2508a4b08d04c27c3db30da88b3b26e67b
AlissonKawan <rm567598@fiap.com.br>
2026-04-09 21:01:21 -0300

    limpeza do projeto (gitignore)

---
+++ Projetos/ProjetoTDBResponde/src/connection/ConnectionFactory.java
@@ -0,0 +1,20 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    private static final String URL = "";
+    private static final String USUARIO = "";
+    private static final String SENHA = "";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao conectar: " + e.getMessage());
+        }
+    }
+}
---
+++ Projetos/Zoologico/src/Animal.java
@@ -0,0 +1,16 @@
+public abstract class Animal {
+
+    private String nome;
+    private String raca;
+    private boolean domestico;
+
+
+    public Animal(String nome, String raca, boolean domestico) {
+        this.nome = nome;
+        this.raca = raca;
+        this.domestico = domestico;
+    }
+
+
+
+}
